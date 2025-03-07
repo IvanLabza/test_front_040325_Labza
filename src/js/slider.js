84203000cc089add9737cad7366ca6db63dc10f0
IvanLabza <100775210+IvanLabza@users.noreply.github.com>
2025-03-07 08:48:36 +0200

    fix head

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,5 +1,5 @@
 let autoplay = false;
-const time = 8000;
+const time = 5000;
 
 if (window.innerWidth >= 765) {
   autoplay = true;
@@ -22,10 +22,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const indicator = document.createElement('div');
     indicator.classList.add('slider-indicator');
     indicator.innerHTML = `
-      <span class="control_prev"> < </span>
+      <span class="control_prev"> <img src="./images/control_prev.png" /> </span>
       <span class="current">${currentIndex}</span> / 
       <span class="total">${slideCount}</span>
-      <span class="control_next"> > </span>
+      <span class="control_next"> <img  src="./images/control_next.png"/> </span>
     `;
     slider.appendChild(indicator);
 
